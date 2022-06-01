1894ba14fe475ac9082ac1a9dd2fd9a3ffb5567c
balakarthi97 <balakarthi199688@gmail.com>
2022-06-01 10:13:58 +0530

    New Add

---
+++ src/org/firstfloor/Flipkart.java
@@ -0,0 +1,80 @@
+package org.firstfloor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Flipkart {
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bala karthi\\eclipse-workspace\\SeleniumProject\\driver\\chromedriver.exe");
+		
+		// Launch website
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.flipkart.com/");
+
+		// Maximize
+		  driver.manage().window().maximize();
+		
+		  WebElement log = driver.findElement(By.xpath("(//input [@type = 'text'])[2]"));
+		 log.sendKeys("9688593090");
+		 
+		  WebElement pass = driver.findElement(By.xpath("//input [@type = 'password']"));
+		 pass.sendKeys("bala@123");
+		 
+		 WebElement logIn = driver.findElement(By.xpath("(//span[text() = 'Login'])[2]")); logIn.click();
+		 
+		 Thread.sleep(3000);
+		 
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 
+		 WebElement mobile = driver.findElement(By.xpath("//input [@type = 'text']")); 
+		 mobile.sendKeys("Mobile");
+		 mobile.sendKeys(Keys.ENTER);
+		 
+		 Thread.sleep(3000);
+		 
+		 WebElement mobilePrice = driver.findElement(By.xpath("(//select[@class= '_2YxCDZ'])[2]"));
+		 mobilePrice.click();
+		 
+		 Select s = new Select(mobilePrice);
+		 s.selectByValue("10000");
+		 
+		 WebElement modelPrice = driver.findElement(By.xpath("//div [@class = '_3sckoD']"));
+		  System.out.println("Mobile Price is: " + modelPrice.getText());
+		 
+
+		  Thread.sleep(3000);
+		  WebElement ram = driver.findElement(By.xpath("//div [text() = '1GB and Below']"));
+		  ram.click();
+		  System.out.println("Ram: " + ram.getText());
+		  
+		  Thread.sleep(3000);
+		  WebElement model = driver.findElement(By.xpath("(//input [@type = 'text'])[2]"));
+		  model.sendKeys("ringme");
+		  
+		  Thread.sleep(3000);
+		  WebElement modelType = driver.findElement(By.xpath("//div [text()= 'ringme']"));
+		  modelType.click();
+		  System.out.println("Mobile Type: " + modelType.getText());
+		  
+		  TakesScreenshot ts = (TakesScreenshot)driver;
+
+		  File sc = ts.getScreenshotAs(OutputType.FILE);
+		  File pa = new File ("C:\\Users\\Bala karthi\\OneDrive\\Documents\\Git Projects\\Colege 2\\College\\Scnap\\ FlipKart.png");
+		  FileUtils.copyFile(sc, pa);
+
+}
+}
